bef4522825de75fb69ab7b2af6de2c8085b248ef
Oscar <oscarblanq@gmail.com>
2020-03-12 03:40:14 +0100

    Tabs bar actualizada

--- jinkobot/src/app/pages/tabs/tabs-routing.module.ts
+++ jinkobot/src/app/pages/tabs/tabs-routing.module.ts
@@ -1,53 +1,42 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { TabsPage } from './tabs.page';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { TabsPage } from "./tabs.page";
 
 const routes: Routes = [
   {
-    path: 'tabs',
-    component: TabsPage,
+    path: "connect",
     children: [
       {
-        path: 'connect',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../connect/connect.module').then(m => m.ConnectPageModule)
-          }
-        ]
-      },
-      {
-        path: 'move',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../play/play.module').then(m => m.MovePageModule)
-          }
-        ]
-      },
-      {
-        path: 'settings',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../settings/settings.module').then(m => m.SettingsPageModule)
-          }
-        ]
-      },
-      {
-        path: '',
-        redirectTo: '/tabs/connect',
-        pathMatch: 'full'
+        path: "",
+        loadChildren: () =>
+          import("../connect/connect.module").then(m => m.ConnectPageModule)
       }
     ]
   },
   {
-    path: '',
-    redirectTo: '/tabs/connect',
-    pathMatch: 'full'
+    path: "play",
+    children: [
+      {
+        path: "",
+        loadChildren: () =>
+          import("../play/play.module").then(m => m.MovePageModule)
+      }
+    ]
+  },
+  {
+    path: "settings",
+    children: [
+      {
+        path: "",
+        loadChildren: () =>
+          import("../settings/settings.module").then(m => m.SettingsPageModule)
+      }
+    ]
+  },
+  {
+    path: "",
+    redirectTo: "connect",
+    pathMatch: "full"
   }
 ];
 
