0eb4573ae1031d13d6cc41a5787808074bd5fbc9
oscarblanq <oscarblanq@gmail.com>
2020-04-23 00:19:57 +0200

    implemented AddRobotButton Component

---
+++ jinkobot/src/app/components/add-robot-button/add-robot-button.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-add-robot-button",
+  templateUrl: "./add-robot-button.component.html",
+})
+export class AddRobotButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
