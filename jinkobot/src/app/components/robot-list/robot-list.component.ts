1b2adb225901c98576f886ddc6fc94fabf7ed1b3
oscarblanq <oscarblanq@gmail.com>
2020-04-23 03:09:45 +0200

    implemented RobotList Component

---
+++ jinkobot/src/app/components/robot-list/robot-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input, OnChanges } from "@angular/core";
+import { IRobot } from "src/app/models/IRobot";
+import { Observable } from "rxjs";
+import { EditRobotPage } from "src/app/pages/edit-robot/edit-robot.page";
+import { ModalController } from "@ionic/angular";
+import { StorageService } from "src/app/services/storage.service";
+
+@Component({
+  selector: "app-robot-list",
+  templateUrl: "./robot-list.component.html",
+})
+export class RobotListComponent implements OnInit, OnChanges {
+  @Input() robot$: Observable<IRobot[]>;
+  robots: IRobot[];
+  constructor(
+    private modalCtrl: ModalController,
+    private storage: StorageService
+  ) {
+    this.robots = [];
+  }
+
+  ngOnInit() {
+    this.robot$.subscribe((data: IRobot[]) => {
+      this.robots = data;
+    });
+  }
+  ngOnChanges() {
+    this.ngOnInit();
+  }
+  async presentModal(robot: IRobot) {
+    const modal = await this.modalCtrl.create({
+      component: EditRobotPage,
+      componentProps: {
+        robot,
+      },
+    });
+    await modal.present();
+    const { data } = await modal.onDidDismiss();
+    this.closeModal(data.savedChanges);
+  }
+
+  async closeModal(data: boolean) {
+    data && (this.robot$ = this.storage.getRobots());
+  }
+}
