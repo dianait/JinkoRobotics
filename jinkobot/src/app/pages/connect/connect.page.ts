779563d25a6f8bf114d1e0deae8134b036093b1b
oscarblanq <oscarblanq@gmail.com>
2020-04-25 00:22:52 +0200

    Added icon to RobotCard Component

--- jinkobot/src/app/pages/connect/connect.page.ts
+++ jinkobot/src/app/pages/connect/connect.page.ts
@@ -5,15 +5,22 @@ import { EditRobotPage } from "../../pages/edit-robot/edit-robot.page";
 import { StorageService } from "src/app/services/storage.service";
 import { Observable } from "rxjs";
 import { IRobot } from "src/app/models/IRobot";
+import { QRScanner, QRScannerStatus } from "@ionic-native/qr-scanner/ngx";
 
 @Component({
   templateUrl: "connect.page.html",
 })
 export class ConnectPage {
   robots = new Observable<IRobot[]>();
+  encodedData = "";
+  QRSCANNED_DATA: string;
+  isOn = false;
+  scannedData: {};
+  scannedText: string;
   constructor(
     private modalCtrl: ModalController,
-    private storage: StorageService
+    private storage: StorageService,
+    public qrScanCtrl: QRScanner
   ) {
     this.robots = this.storage.getRobots();
   }
@@ -21,12 +28,49 @@ export class ConnectPage {
     const modal = await this.modalCtrl.create({
       component: EditRobotPage,
       componentProps: {
-        robot: { id: "123", alias: "" },
+        robot: { id: this.scannedText, alias: "" },
       },
     });
     await modal.present();
     const { data } = await modal.onDidDismiss();
     this.closeModal(data.savedChanges);
+  }
+
+  goToQrScan() {
+    this.qrScanCtrl
+      .prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+          this.isOn = true;
+
+          // start scanning
+          const scanSub = this.qrScanCtrl.scan().subscribe((text: string) => {
+            console.log("Scanned something", text);
+            this.isOn = false;
+
+            this.QRSCANNED_DATA = text;
+            if (this.QRSCANNED_DATA !== "") {
+              this.scannedText = text;
+              this.closeScanner();
+              scanSub.unsubscribe();
+              this.presentModal();
+            }
+          });
+          this.qrScanCtrl.show();
+        } else if (status.denied) {
+          console.log("camera permission denied");
+          this.qrScanCtrl.openSettings();
+        } else {
+        }
+      })
+      .catch((e: any) => console.log("Error is", e));
+  }
+
+  closeScanner() {
+    this.isOn = false;
+    this.qrScanCtrl.hide();
+    this.qrScanCtrl.destroy();
   }
 
   async closeModal(data: boolean) {
