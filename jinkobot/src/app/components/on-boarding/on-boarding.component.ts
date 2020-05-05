b5c52e9a860d7028e3a9ce6b9b09e2101cc096da
oscarblanq <oscarblanq@gmail.com>
2020-05-05 10:58:42 +0200

    modified OnBoardingComponent

--- jinkobot/src/app/components/on-boarding/on-boarding.component.ts
+++ jinkobot/src/app/components/on-boarding/on-boarding.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { StorageService } from "src/app/services/storage.service";
 import { Router } from "@angular/router";
 
